91b54b449e61759ea107419352af55cf2f67d22f
nouhayla <94287848+Alphavenom1@users.noreply.github.com>
2022-07-12 06:36:25 +0200

    Create Data types & Missing values.py

---
+++ Data types & Missing values.py
@@ -0,0 +1,25 @@
+#setup
+import pandas as pd
+
+reviews = pd.read_csv("../input/wine-reviews/winemag-data-130k-v2.csv", index_col=0)
+
+from learntools.core import binder; binder.bind(globals())
+from learntools.pandas.data_types_and_missing_data import *
+print("Setup complete.")
+
+#What is the data type of the points column in the dataset?
+dtype = reviews.points.dtype
+print(dtype) #int64
+
+#Create a Series from entries in the points column, but convert the entries to strings. 
+point_strings = reviews['points'].astype('str')
+
+#Sometimes the price column is null. How many reviews in the dataset are missing a price?
+n_missing_prices =pd.isnull(reviews.price).sum()
+print(n_missing_prices) #8996
+
+#What are the most common wine-producing regions?
+
+
+
+#
